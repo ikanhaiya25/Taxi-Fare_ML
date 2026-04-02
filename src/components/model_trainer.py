8f8eaa9f7039f61aa36c9a043af15c837310bf97
Kanhaiya <kanhaiyaschaudhari@gmail.com>
2026-04-02 21:23:21 +0530

    model trainer update

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,33 +1,37 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.pipeline import Pipeline
 from src.utils.common import save_object
 from src.logger import logging
 
 class ModelTrainer:
 
-    def __init__(self,config):
+    def __init__(self, config):
+        self.config = config
 
-        self.config=config
-
-
-    def initiate_model_trainer(self,X_train,X_test,y_train,y_test):
+    def initiate_model_trainer(self, X_train, X_test, y_train, y_test, preprocessor):
         logging.info("Model training started")
 
-        model=DecisionTreeRegressor(
+        model = DecisionTreeRegressor(
             random_state=self.config.random_state
         )
 
-        model.fit(X_train,y_train)
-        logging.info("Model fitted successfully")
+        pipeline = Pipeline([
+            ("preprocessor", preprocessor),
+            ("model", model)
+        ])
 
-        preds=model.predict(X_test)
+        pipeline.fit(X_train, y_train)
+        logging.info("Pipeline fitted successfully")
 
-        r2 = r2_score(y_test,preds)
-        mae = mean_absolute_error(y_test,preds)
-        mse = mean_squared_error(y_test,preds)
+        preds = pipeline.predict(X_test)
+
+        r2 = r2_score(y_test, preds)
+        mae = mean_absolute_error(y_test, preds)
+        mse = mean_squared_error(y_test, preds)
 
         logging.info(f"DecisionTreeRegressor Metrics: R2={r2}, MAE={mae}, MSE={mse}")
-        print(f"DecisionTreeReressor Metrics:\nR2 Score: {r2}\nMAE: {mae}\nMSE: {mse}")
+        print(f"DecisionTreeRegressor Metrics:\nR2 Score: {r2}\nMAE: {mae}\nMSE: {mse}")
 
-        save_object(self.config.model_path,model)
-        logging.info("Model saved successfully")
+        save_object(self.config.model_path, pipeline)
+        logging.info("Pipeline saved successfully")
